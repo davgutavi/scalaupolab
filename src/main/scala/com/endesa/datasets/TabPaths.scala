0660b22cc7db1607b3e19d36868093f03a71a0a9
davgutavi <davgutavi@gmail.com>
2017-03-22 16:38:42 +0100

    Commit

--- src/main/scala/com/endesa/datasets/TabPaths.scala
+++ src/main/scala/com/endesa/datasets/TabPaths.scala
@@ -5,93 +5,99 @@ package com.endesa.datasets
   */
 object TabPaths {
 
+  //  final val prefix = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/"
+  final val prefix = "/mnt/4f9c736f-9893-4dd7-b726-0df95240567f/ENDESA/endesa_descomprimido/"
+
+
   //Maestro Contratos
-  final val TAB_00C = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_00C/Endesa_TAB_00C_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_00C_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_00C/TAB_00C_headers.csv"
+  final val TAB_00C = prefix+"TAB_00C/Endesa_TAB_00C_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_00C_headers = prefix+"TAB_00C_headers.csv"
 
   //Maestro Aparatos
-  final val TAB_00E = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_00E/Endesa_TAB_00E_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_00E_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_00E/TAB_00E_headers.csv"
+  final val TAB_00E = prefix+"TAB_00E/Endesa_TAB_00E_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_00E_headers = prefix+"endesa_descomprimido/TAB_00E/TAB_00E_headers.csv"
 
   //Curvas de Carga
-  final val TAB_01_10 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20100101_20101231.csv"
-  final val TAB_01_11 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20110101_20111231.csv"
-  final val TAB_01_12 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20120101_20121231.csv"
-  final val TAB_01_13 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20130101_20131231.csv"
-  final val TAB_01_14 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20140101_20141231.csv"
-  final val TAB_01_15 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20150101_20151231.csv"
-  final val TAB_01_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/Endesa_TAB_01_20170127_CZZ_20160101_20161231.csv"
-  final val TAB_01_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_01/TAB_01_headers.csv"
-
-  //Bits de calidad
-  final val TAB_02_10 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_A2010_20170127_CZZ.csv"
-  final val TAB_02_11 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_A2011_20170127_CZZ.csv"
-  final val TAB_02_12 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_A2012_20170127_CZZ.csv"
-  final val TAB_02_13 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_A2013_20170127_CZZ.csv"
-  final val TAB_02_14 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_20170127_CZZ_20140101_20141231.csv"
-  final val TAB_02_15 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_20170127_CZZ_20150101_20151231.csv"
-  final val TAB_02_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/Endesa_TAB_02_20170127_CZZ_20160101_20161231.csv"
-  final val TAB_02_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_02/TAB_02_headers.csv"
+  final val TAB_01_10 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20100101_20101231.csv"
+  final val TAB_01_11 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20110101_20111231.csv"
+  final val TAB_01_12 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20120101_20121231.csv"
+  final val TAB_01_13 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20130101_20131231.csv"
+  final val TAB_01_14 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20140101_20141231.csv"
+  final val TAB_01_15 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20150101_20151231.csv"
+  final val TAB_01_16 = prefix+"TAB_01/Endesa_TAB_01_20170127_CZZ_20160101_20161231.csv"
+  final val TAB_01_headers = prefix+"TAB_01/TAB_01_headers.csv"
 
   //Consumos de Tipo I - IV
-  final val TAB_03 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_03/Endesa_TAB_03_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_03_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_03/TAB_03_headers.csv"
+  final val TAB_03 = prefix+"TAB_03/Endesa_TAB_03_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_03_headers = prefix+"TAB_03/TAB_03_headers.csv"
 
   //Consumos de Tipo V
-  final val TAB_04_10 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20100101_20101231.csv"
-  final val TAB_04_11 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20110101_20111231.csv"
-  final val TAB_04_12 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20120101_20121231.csv"
-  final val TAB_04_13 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20130101_20131231.csv"
-  final val TAB_04_14 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20140101_20141231.csv"
-  final val TAB_04_15 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20150101_20151231.csv"
-  final val TAB_04_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/Endesa_TAB_04_20170127_CZZ_20160101_20161231.csv"
-  final val TAB_04_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_04/TAB_04_headers.csv"
+  final val TAB_04_10 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20100101_20101231.csv"
+  final val TAB_04_11 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20110101_20111231.csv"
+  final val TAB_04_12 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20120101_20121231.csv"
+  final val TAB_04_13 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20130101_20131231.csv"
+  final val TAB_04_14 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20140101_20141231.csv"
+  final val TAB_04_15 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20150101_20151231.csv"
+  final val TAB_04_16 = prefix+"TAB_04/Endesa_TAB_04_20170127_CZZ_20160101_20161231.csv"
+  final val TAB_04_headers = prefix+"TAB_04/TAB_04_headers.csv"
+
+  //Contratación
+  final val TAB_05A = prefix+"TAB_05A/Endesa_TAB_05A_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_05A_headers = prefix+"TAB_05A/TAB_05A_headers.csv"
+
+  //Geolocalización
+  final val TAB_05B = prefix+"TAB_05B/Endesa_TAB_05B_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_05B_headers = prefix+"TAB_05B/TAB_05B_headers.csv"
 
   //Clientes
-  final val TAB_05C = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_05C/Endesa_TAB_05C_20170126_CZZ_20100101_20161231.csv"
-  final val TAB_05C_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_05C/TAB_05C_headers.csv"
-
-  //Clientes PTOSE
-  final val TAB_05D = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_05D/Endesa_TAB_05D_20170126_CZZ_20100101_20161231.csv"
-  final val TAB_05D_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_05D/TAB_05D_headers.csv"
-
-  //Magnitudes TPL
-  final val TAB_10 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_10/Endesa_TAB_10_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_10_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_10/TAB_10_headers.csv"
-
-  //Facturación
-  final val TAB_12_10_12 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2010_A_2012.csv"
-  final val TAB_12_12_13 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2012_A_2013.csv"
-  final val TAB_12_14_15 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2014_A_2015.csv"
-  final val TAB_12_16_17 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2016_A_2017.csv"
-  final val TAB_12_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_12/TAB_12_headers.csv"
+  final val TAB_05C = prefix+"TAB_05C/Endesa_TAB_05C_20170126_CZZ_20100101_20161231.csv"
+  final val TAB_05C_headers = prefix+"TAB_05C/TAB_05C_headers.csv"
 
   //TDC
-  final val TAB_15A = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15A/Endesa_TAB_15A_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_15A_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15A/TAB_15A_headers.csv"
+  final val TAB_15A = prefix+"TAB_15A/Endesa_TAB_15A_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_15A_headers = prefix+"TAB_15A/TAB_15A_headers.csv"
 
   //Operaciones TDC
-  final val TAB_15B = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15B/Endesa_TAB_15B_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_15B_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15B/TAB_15B_headers.csv"
+  final val TAB_15B = prefix+"TAB_15B/Endesa_TAB_15B_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_15B_headers = prefix+"TAB_15B/TAB_15B_headers.csv"
 
   //Movimientos TDC
-  final val TAB_15C = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15C/Endesa_TAB_15C_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_15C_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_15C/TAB_15C_headers.csv"
+  final val TAB_15C = prefix+"TAB_15C/Endesa_TAB_15C_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_15C_headers = prefix+"TAB_15C/TAB_15C_headers.csv"
 
   //Expedientes
-  final val TAB_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_16/Endesa_TAB_16_20170127_CZZ_20100101_20161231.csv"
-  final val TAB_16_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_16/TAB_16_headers.csv"
+  final val TAB_16 = prefix+"TAB_16/Endesa_TAB_16_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_16_headers = prefix+"TAB_16/TAB_16_headers.csv"
+
+  ///////////************Otros
+
+  //Clientes PTOSE
+  final val TAB_05D = prefix+"TAB_05D/Endesa_TAB_05D_20170126_CZZ_20100101_20161231.csv"
+  final val TAB_05D_headers = prefix+"TAB_05D/TAB_05D_headers.csv"
+
+  //Magnitudes TPL
+  final val TAB_10 = prefix+"TAB_10/Endesa_TAB_10_20170127_CZZ_20100101_20161231.csv"
+  final val TAB_10_headers = prefix+"TAB_10/TAB_10_headers.csv"
+
+  //Facturación
+  final val TAB_12_10_12 = prefix+"TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2010_A_2012.csv"
+  final val TAB_12_12_13 = prefix+"TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2012_A_2013.csv"
+  final val TAB_12_14_15 = prefix+"TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2014_A_2015.csv"
+  final val TAB_12_16_17 = prefix+"TAB_12/Endesa_TAB_12_20170127_CZZ_DE_2016_A_2017.csv"
+  final val TAB_12_headers = prefix+"TAB_12/TAB_12_headers.csv"
+
+
 
   //Interrupciones
-  final val TAB_18_ene_mar_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20160411_XZZ_20160123_20160331_1.csv"
-  final val TAB_18_jul_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20160712_XZZ_1.csv"
-  final val TAB_18_ago_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20160816_XZZ_1.csv"
-  final val TAB_18_sep_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20160921_XZZ_1.csv"
-  final val TAB_18_oct_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20161010_XZZ_1.csv"
-  final val TAB_18_nov_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20161108_XZZ_1.csv"
-  final val TAB_18_dic_16 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20161222_XZZ_1.csv"
-  final val TAB_18_ene_17 = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/C3_Endesa_TAB_18_20170111_XZZ_1.csv"
-  final val TAB_18_headers = "/media/davgutavi/Maxtor/ENDESA/endesa_descomprimido/TAB_18/TAB_18_headers.csv"
+  final val TAB_18_ene_mar_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20160411_XZZ_20160123_20160331_1.csv"
+  final val TAB_18_jul_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20160712_XZZ_1.csv"
+  final val TAB_18_ago_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20160816_XZZ_1.csv"
+  final val TAB_18_sep_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20160921_XZZ_1.csv"
+  final val TAB_18_oct_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20161010_XZZ_1.csv"
+  final val TAB_18_nov_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20161108_XZZ_1.csv"
+  final val TAB_18_dic_16 = prefix+"TAB_18/C3_Endesa_TAB_18_20161222_XZZ_1.csv"
+  final val TAB_18_ene_17 = prefix+"TAB_18/C3_Endesa_TAB_18_20170111_XZZ_1.csv"
+  final val TAB_18_headers = prefix+"TAB_18/TAB_18_headers.csv"
 
 
 
--- src/main/scala/com/entrypoints/MainClientesCurvasV1.scala
+++ src/main/scala/com/entrypoints/MainClientesCurvasV1.scala
@@ -17,51 +17,65 @@ object MainClientesCurvasV1 {
 
     val df_00C = LoadTable.loadTable(TabPaths.TAB_00C,TabPaths.TAB_00C_headers)
     //df_00C.cache()
+    println("Persistiendo Contratos\n")
     df_00C.persist(StorageLevel.MEMORY_ONLY)
     df_00C.registerTempTable("contratos")
 
     val df_05C = LoadTable.loadTable(TabPaths.TAB_05C,TabPaths.TAB_05C_headers)
 //    df_05C.cache()
+      println("Persistiendo Clientes\n")
       df_05C.persist(StorageLevel.MEMORY_ONLY)
     df_05C.registerTempTable("clientes")
 
     val df_00E = LoadTable.loadTable(TabPaths.TAB_00E,TabPaths.TAB_00E_headers)
 //    df_00E.cache()
+      println("Persistiendo Aparatos\n")
       df_00E.persist(StorageLevel.MEMORY_ONLY)
     df_00E.registerTempTable("aparatos")
 
     val df_01_10 = LoadTable.loadTable(TabPaths.TAB_01_10,TabPaths.TAB_01_headers)
 //    df_01_10.cache()
+      println("Persistiendo Cargas\n")
       df_01_10.persist(StorageLevel.MEMORY_ONLY)
       df_01_10.registerTempTable("cargas")
 
+      println("Construyendo J1\n")
     val j1 = sql(
       """SELECT contratos.origen, contratos.cemptitu, contratos.ccontrat, contratos.cnumscct, contratos.cupsree2, contratos.cpuntmed, clientes.ccliente, clientes.dapersoc, clientes.dnombcli
          FROM contratos JOIN clientes
          ON contratos.origen = clientes.origen AND contratos.cemptitu = clientes.cemptitu AND contratos.ccontrat = clientes.ccontrat AND contratos.cnumscct = clientes.cnumscct
       """)
 
+      println("Persistiendo J1\n")
     j1.cache()
+    println("Registrando Tabla J1\n")
     j1.registerTempTable("con_cli")
-    println("\nJoin contratos-clientes ("+j1.count()+" registros)\n")
+
+      println("\nJoin contratos-clientes ("+j1.count()+" registros)\n")
     j1.show(5)
-    df_00C.unpersist()
+
+      println("Borrando Contratos, Clientes\n")
+      df_00C.unpersist()
     df_05C.unpersist()
 
+      println("Construyendo J2\n")
     val j2 = sql(
       """SELECT aparatos.origen, aparatos.cpuntmed, con_cli.ccliente, con_cli.dapersoc, con_cli.dnombcli
          FROM con_cli JOIN aparatos
          ON con_cli.origen = aparatos.origen AND con_cli.cupsree2 = aparatos.cupsree2 AND con_cli.cpuntmed = aparatos.cpuntmed
       """)
-
+      println("Persistiendo J2\n")
     j2.cache()
+      println("Registrando Tabla J2\n")
     j2.registerTempTable("con_cli_apa")
-    println("\nJoin contratos-clientes-aparatos ("+j2.count()+" registros)\n")
+
+      println("\nJoin contratos-clientes-aparatos ("+j2.count()+" registros)\n")
     j2.show(5)
+      println("Borrando Aparatos, J1\n")
       df_00E.unpersist()
       j1.unpersist()
 
-
+      println("Construyendo J3\n")
     val j3 = sql(
       """SELECT cargas.origen, cargas.cpuntmed, con_cli_apa.ccliente, con_cli_apa.dapersoc, con_cli_apa.dnombcli,
          cargas.hora_01, cargas.1q_consumo_01, cargas.2q_consumo_01, cargas.3q_consumo_01, cargas.3q_consumo_01
@@ -70,7 +84,7 @@ object MainClientesCurvasV1 {
       """)
 
 
-
+      println("Persistiendo J3\n")
     j3.cache()
     println("\nJoin contratos-clientes-aparatos-curvas ("+j3.count()+" registros)\n")
     j3.show(5)
