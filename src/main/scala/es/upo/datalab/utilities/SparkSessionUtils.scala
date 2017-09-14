b3ba92dfcb3d5d507dfd7c4231f5235825e71859
davgutavi <davgutavi@gmail.com>
2017-09-14 11:11:53 +0200

    Commit

--- src/main/scala/es/upo/datalab/utilities/SparkSessionUtils.scala
+++ src/main/scala/es/upo/datalab/utilities/SparkSessionUtils.scala
@@ -12,23 +12,26 @@ object SparkSessionUtils {
   Logger.getLogger("akka").setLevel(Level.OFF)
 
   //############################Configuración en intellij
-   val conf = new SparkConf()
-      .setMaster("local[*]")
-      .setAppName("endesa_modo_desarrollo")
-      .set("spark.local.dir","/mnt/datos/tempSpark")
-      .set("spark.network.timeout","10000000")
-      .set("spark.executor.heartbeatInterval","10000")
-      .set("spark.executor.memory","20g")
-      .set("spark.driver.memory","10g")
-      .set("spark.executor.memory","10g")
+//   val conf = new SparkConf()
+//      .setMaster("local[*]")
+//      .setAppName("endesa_modo_desarrollo")
+//      .set("spark.local.dir","/mnt/datos/tempSpark")
+//      .set("spark.network.timeout","10000000")
+//      .set("spark.executor.heartbeatInterval","10000")
+//      .set("spark.executor.memory","20g")
+//      .set("spark.driver.memory","10g")
+//      .set("spark.executor.memory","10g")
 
 
   //############################Submit desde gutierrez en cluster mode
-//  val conf = new SparkConf()
-//    .setMaster("mesos://192.168.47.247:5050")
-//    .setAppName("tab24n_modo_cluster_david")
-//    .set("spark.local.dir","hdfs://192.168.47.247/user/tempSpark")
-////    .set("spark.cores.max", "48")
+  val conf = new SparkConf()
+    .setMaster("mesos://192.168.47.247:5050")
+    .setAppName("tab24n_modo_cluster_david")
+    .set("spark.local.dir","hdfs://192.168.47.247/user/tempSpark")
+    .set("spark.cores.max", "48")
+
+
+
 
   //############################Submit desde gutierrez en client mode
 //    val conf = new SparkConf()
@@ -36,9 +39,7 @@ object SparkSessionUtils {
 //      .setAppName("endesa_modo_cliente")
 //      .set("spark.local.dir","hdfs://192.168.47.247/user/tempSpark")
 //      .set("spark.cores.max", "48")
-//      .set("spark.executor.uri", "hdfs://192.168.47.247/user/spark-2.1.1-bin-hadoop2.6.tgz")
-
-
+    .set("spark.executor.uri", "hdfs://192.168.47.247/user/spark-2.1.1-bin-hadoop2.6.tgz")
 
 
     val session = SparkSession.builder().config(conf).getOrCreate()
