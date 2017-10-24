510b66b20b55d0108fa6847dd4db17b7a107143c
davgutavi <davgutavi@gmail.com>
2017-10-24 14:05:24 +0200

    Merge branch 'master' of https://github.com/davgutavi/scalaupolab
    # Conflicts:
    #	.idea/modules/root-build.iml

--- src/main/scala/es/upo/datalab/datamining/FnoFdiarios.scala
+++ src/main/scala/es/upo/datalab/datamining/FnoFdiarios.scala
@@ -1,9 +1,12 @@
 package es.upo.datalab.datamining
 
 import es.upo.datalab.utilities.{LoadTableParquet, SparkSessionUtils}
-import org.apache.spark.ml.classification.GBTClassifier
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
 import org.apache.spark.ml.feature._
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
+import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.storage.StorageLevel
 
 
@@ -22,117 +25,85 @@ object FnoFdiarios {
 
     println("Cargando DataFrames")
 
-    val data = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/ds_123_diario")
+    val dataaux = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/ds_123_diario")
 
-//   val trainingData = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/train_123_diario")
-//
-//   val testData = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/test_123_diario")
+    val data = dataaux.withColumnRenamed("fraude", "label")
 
+    val trainingData = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/train_123_diario")
 
-    val dataAux = data.drop("fraude")
-
-    val featureCols: Array[String] = dataAux.columns
+    val testData = LoadTableParquet.loadTable("hdfs://10.141.0.224:9000/fabregas/endesa/datasets_parquet/test_123_diario")
 
 
-//    val a2 = featureCols.filter(_!="cpuntmed").filter(_!="ccodpost").filter(_!="cenae")
+    //INDEXERS
 
-    //tranformor to convert string to category values
-//    val cpuntmedIndexer = new StringIndexer().setInputCol("cpuntmed").setOutputCol("cpuntmedCat")
     val ccodpostIndexer = new StringIndexer().setInputCol("ccodpost").setOutputCol("ccodpostCat")
+
     val cnaeIndexer = new StringIndexer().setInputCol("cenae").setOutputCol("cenaeCat")
 
-//    val e1:StringIndexerModel = cpuntmedIndexer.fit(data)
-    val e2:StringIndexerModel = ccodpostIndexer.fit(data)
-    val e3:StringIndexerModel = cnaeIndexer.fit(data)
 
-//    val d1 = e1.transform(data)
-    val d2 = e2.transform(data)
-    val d3 = e3.transform(d2)
+    //ASSEMBLER
+    val fdat= data.drop("cpuntmed","ccodpost", "cenae","label").columns
+
+    val featureAssembler = new VectorAssembler().setInputCols(fdat).setOutputCol("rawFeatures")
 
 
+    //FEATURE INDEXER
+    //val featureIndexer = new VectorIndexer().setInputCol("rawFeatures").setOutputCol("indexedFeatures").setMaxCategories(1)
 
-    val fdat= d3.drop("cpuntmed","ccodpost", "cenae","fraude")
+    //MODEL
+     val gbt = new GBTClassifier().setLabelCol("label").setFeaturesCol("rawFeatures").setMaxIter(10)
 
-    val featureAssembler = new VectorAssembler().setInputCols(fdat.columns).setOutputCol("rawFeatures")
-
-    val dataWithFeatures:DataFrame = featureAssembler.transform(d3)
-
-
-    val featureIndexer = new VectorIndexer()
-      .setInputCol("rawFeatures")
-      .setOutputCol("indexedFeatures")
-//      .setMaxCategories(1)
-      .fit(dataWithFeatures)
+//     val gbt = new GBTClassifier().setLabelCol("fraude").setFeaturesCol("rawFeatures").setMaxIter(10)
 
 
-    val d4 = featureIndexer.transform(dataWithFeatures)
+    //CONVERTERS
+    val ccodpostConverter = new IndexToString().setInputCol("ccodpostCat").setOutputCol("ccodpost_conv")
 
-    d4.show(10)
+    val cnaeConverter = new IndexToString().setInputCol("cenaeCat").setOutputCol("cenae_conv")
 
-    val Array (training, test) = d4.randomSplit(Array(0.7, 0.3))
-
-
-
-
-    println("Entrenando modelo")
-        // Train a GBT model.
-        val gbt = new GBTClassifier()
-          .setLabelCol("fraude")
-          .setFeaturesCol("indexedFeatures")
-//          .setMaxBins(3629)
-          .setMaxIter(10)
-          .fit(training)
+    //PIPELINE
+    val pipeline = new Pipeline().setStages(Array(ccodpostIndexer, cnaeIndexer, featureAssembler, gbt, ccodpostConverter, cnaeConverter))
 
 
+    //TRAINING Y TEST
 
-    val predictions = gbt.transform(test)
+    val Array(training, test) = data.randomSplit(Array(0.7,0.3))
 
-    predictions.select("prediction", "fraude", "indexedFeatures").show(5)
-
-
-//    // Train a GBT model.
-//    val gbt = new GBTClassifier()
-//      .setLabelCol("fraude")
-//      .setFeaturesCol("features")
-//      .setMaxIter(10)
-//      .fit(data)
+//    //ENTRENAMIENTO
 //
+//    val model = pipeline.fit(training)
 //
+//    //TEST
 //
-////    // Convert indexed labels back to original labels.
-////    val labelConverter = new IndexToString()
-////      .setInputCol("prediction")
-////      .setOutputCol("predictedLabel")
-////      .setLabels(labelIndexer.labels)
+//    val predictions = model.transform(test)
 //
+//    //PREDICCIONES
 //
-//    // Chain indexers and GBT in a Pipeline
-//    val pipeline = new Pipeline()
-//      .setStages(Array(labelIndexer, featureIndexer, gbt, labelConverter))
-//
-//
-//    // Train model.  This also runs the indexers.
-//    val model = pipeline.fit(trainingData)
-//
-//    // Make predictions.
-//    val predictions = model.transform(testData)
-//
-//    // Select example rows to display.
-//    predictions.select("predictedLabel", "label", "features").show(5)
-//
-//    // Select (prediction, true label) and compute test error
-//    val evaluator = new MulticlassClassificationEvaluator()
-//      .setLabelCol("indexedLabel")
-//      .setPredictionCol("prediction")
-//      .setMetricName("precision")
-//
-//    val accuracy = evaluator.evaluate(predictions)
-//    println("Test Error = " + (1.0 - accuracy))
-//
-//
-//    val gbtModel = model.stages(2).asInstanceOf[GBTClassificationModel]
-//
-//    println("Learned classification GBT model:\n" + gbtModel.toDebugString)
+//    predictions.select("prediction", "fraude", "rawFeatures").show(20)
+
+
+    //EVALUATION
+
+
+    val paramGrid = new ParamGridBuilder().addGrid(gbt.maxIter, Array(1, 2)).addGrid(gbt.maxDepth, Array(1, 2)).build()
+
+
+
+    val cv = new CrossValidator().setEstimator(pipeline).setEvaluator(new BinaryClassificationEvaluator).setEstimatorParamMaps(paramGrid).setNumFolds(4)  // Use 3+ in practice
+
+    val cvModel = cv.fit(training)
+
+
+//    cvModel.transform(test).select("prediction","label","probability","rawFeatures").show(40)
+
+    cvModel.transform(test).select("prediction","label","probability").show(40)
+
+
+    cvModel.write.overwrite().save("/Users/davgutavi/Desktop/model")
+
+    //GUARDAR MODELO
+
+
 
 
     SparkSessionUtils.session.stop()
