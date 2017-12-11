dc7dae58b8c3d66c6555dfc0a4d35eeb8aaf40ca
davgutavi <davgutavi@gmail.com>
2017-12-11 10:18:44 +0100

    Merge branch 'master' of https://github.com/davgutavi/scalaupolab
    # Conflicts:
    #	.idea/modules/root-build.iml

--- src/main/scala/es/upo/datalab/entrypoints/datamining/Clustering.scala
+++ src/main/scala/es/upo/datalab/entrypoints/datamining/Clustering.scala
@@ -17,80 +17,87 @@ object Clustering  {
 
     TimingUtils.time {
 
-      val output1 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_raw_umr"
+      val outputTest = "/Users/davgutavi/Desktop/endesa/clustering/test"
 
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_raw_umr_macDavid, output1, k, maxIter, initializationSteps, tolerance, seed)
+      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_raw_umr_macDavid, outputTest, Array(2), Array(20), Array(2), Array(0.0001), Array(1L))
 
-      println("Experiment 1 done")
-
-      val output2 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_raw_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_raw_con_macDavid, output2, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 2 done")
-
-      val output3 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_max_umr"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_max_umr_macDavid, output3, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 3 done")
-
-      val output4 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_max_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_max_con_macDavid, output4, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 4 done")
+      println("Test done")
 
 
-      val output5 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_slo_umr"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_slo_umr_macDavid, output5, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 5 done")
-
-      val output6 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_slo_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_slo_con_macDavid, output6, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 6 done")
-
-
-      val output7 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_raw_umr"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_raw_umr_macDavid, output7, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 7 done")
-
-      val output8 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_raw_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_raw_con_macDavid, output8, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 8 done")
-
-      val output9 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_max_umr"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_max_umr_macDavid, output9, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 9 done")
-
-      val output10 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_max_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_max_con_macDavid, output10, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 10 done")
-
-
-      val output11 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_slo_umr"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_slo_umr_macDavid, output11, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 11 done")
-
-      val output12 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_slo_con"
-
-      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_slo_con_macDavid, output12, k, maxIter, initializationSteps, tolerance, seed)
-
-      println("Experiment 12 done")
+//      val output1 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_raw_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_raw_umr_macDavid, output1, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 1 done")
+//
+//      val output2 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_raw_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_raw_con_macDavid, output2, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 2 done")
+//
+//      val output3 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_max_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_max_umr_macDavid, output3, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 3 done")
+//
+//      val output4 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_max_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_max_con_macDavid, output4, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 4 done")
+//
+//
+//      val output5 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_slo_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_slo_umr_macDavid, output5, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 5 done")
+//
+//      val output6 = "/Users/davgutavi/Desktop/endesa/clustering/lc_454d_slo_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p454d_slo_con_macDavid, output6, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 6 done")
+//
+//
+//      val output7 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_raw_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_raw_umr_macDavid, output7, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 7 done")
+//
+//      val output8 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_raw_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_raw_con_macDavid, output8, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 8 done")
+//
+//      val output9 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_max_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_max_umr_macDavid, output9, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 9 done")
+//
+//      val output10 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_max_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_max_con_macDavid, output10, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 10 done")
+//
+//
+//      val output11 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_slo_umr"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_slo_umr_macDavid, output11, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 11 done")
+//
+//      val output12 = "/Users/davgutavi/Desktop/endesa/clustering/lc_364d_slo_con"
+//
+//      EndesaKmeans.clusteringExperiment(DatasetPaths.p364d_slo_con_macDavid, output12, k, maxIter, initializationSteps, tolerance, seed)
+//
+//      println("Experiment 12 done")
 
     }
 
